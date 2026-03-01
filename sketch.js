fdfa04e705830a34c4a74aa80aa1bb24872a3421
TheKingsCrown <stevenmars77@gmail.com>
2026-02-28 23:51:49 -0800

    added button functionality, also learned a lesson about git

--- sketch.js
+++ sketch.js
@@ -1,23 +1,53 @@
-var gridSize = 16;
+let defaultGridSize = 16;
+
+let button = document.querySelector("#grid-button");
+button.addEventListener("click", createNewGrid);
+
 function createGrid(size) {
+    let grid = document.createElement("div");
+    grid.classList.add("grid");
+    
     let gridContainer = document.querySelector(".grid-container");
+    gridContainer.appendChild(grid);
+
+    // seperated row creation and grid-square creation to avoid nesting too deeply.
     for (let i = 0; i < size; i++) {
-            gridContainer.appendChild((() => {
-            var row = document.createElement("div");
-            row.classList.add("grid-row");
-            return row;
+            grid.appendChild((() => {
+            var gridRow = document.createElement("div");
+            gridRow.classList.add("grid-row");
+            return gridRow;
         })());
     }
-    gridContainer.childNodes.forEach(row => {
+    grid.childNodes.forEach(gridRow => {
         for (let i = 0; i < size; i++) {
-            row.appendChild((() => {
+            gridRow.appendChild((() => {
                 var gridSquare = document.createElement("div");
                 gridSquare.classList.add("grid-square");
-                gridSquare.style.border = "1px solid #000000"
+                gridSquare.addEventListener("mouseenter", (event) => {
+                    event.target.classList.add("fill"); // Add fill class to turn square black
+                });
                 return gridSquare;
             })());
         }
     });
 }
 
-createGrid(gridSize);
+function destroyGrid() {
+    let gridContainer = document.querySelector(".grid");
+    if (!gridContainer) {
+        return;
+    }
+    gridContainer.remove();
+}
+
+function createNewGrid() {
+    var gridSize = Number(prompt("choose a grid size between 1-100"));
+    while (gridSize > 100 || gridSize < 1 || Number.isNaN(gridSize)) {
+        gridSize = Number(prompt("choose a grid size between 1-100"));
+    }
+
+    destroyGrid();
+    createGrid(gridSize);
+}
+
+createGrid(defaultGridSize);
